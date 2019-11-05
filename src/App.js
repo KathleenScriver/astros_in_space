ddfabf2dd5b084a756c7b3fba139e79a29306400
KathleenScriver <klescriver@gmail.com>
2019-11-05 15:25:27 -0700

    Adds astros names.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,38 @@
-// create your App component here
+import React, { Component } from 'react';
+import Astro from './Astro';
+
+export default class App extends Component {
+    state = {
+        astros: []
+    }
+    
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json())
+        .then(info => this.setAstros(info["people"]))
+    }
+    
+    setAstros = (astros) => {
+        const allAstros = astros.map(astro => {
+            return astro.name
+        })
+
+        this.setState({
+            astros: allAstros
+        })
+    }
+
+    displayAstros = () => {
+        return this.state.astros.map(astro => {
+            return <Astro name={astro} />
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.displayAstros()}
+            </div>
+        )
+    }
+}
